fc930c6fa215ce0f523505b0e63c7a1bf2da5b9b
Diego F Orejuela <diegoforejuelao@gmail.com>
2018-09-28 16:44:56 -0500

    Implemented First four JUnit test cases from help video

--- SkunkProject/src/Die.java
+++ SkunkProject/src/Die.java
@@ -2,6 +2,9 @@
 public class Die
 {
 	private int lastRoll;
+	private boolean predictable = false;
+	private int[] rolls;
+	private int nextRoll_Index;
 
 	public Die()
 	{
@@ -14,15 +17,34 @@ public class Die
 		return this.lastRoll;
 	}
 
-	public void roll() // note how this changes Die's state, but doesn't return anything
+	public void roll() // note how this changes Die's state, but doesn't return
+						// anything
 	{
-		this.lastRoll = (int) (Math.random() * 6 + 1);
+		if (!predictable)
+			this.lastRoll = (int) (Math.random() * 6 + 1);
+		else
+		{
+			this.lastRoll = this.rolls[nextRoll_Index];
+			nextRoll_Index++;
+
+			if (nextRoll_Index >= this.rolls.length)
+			{
+				nextRoll_Index = 0;
+			}
+		}
 	}
-	
+
 	@Override
 	public String toString() // this OVERRIDES the default Object.toString()
 	{
 		return "Die: " + this.getLastRoll();
 	}
 
+	// Method for JUnit Testing
+	public Die(int[] predictable_Rolls)
+	{
+		this.predictable = true;
+		this.rolls = predictable_Rolls;
+		this.nextRoll_Index = 0;
+	}
 }
--- SkunkProject/test/TestDie.java
+++ SkunkProject/test/TestDie.java
@@ -20,10 +20,55 @@ public class TestDie
 	}
 
 	@Test
-	public void test()
+	public void test_Init_PredictableDie()
 	{
-		fail("Not yet implemented");
+		int[] initValues = new int[]
+		{ 3, 2, 1 };
+
+		Die die = new Die(initValues);
+		die.roll();
+		int value = die.getLastRoll();
+
+		assertEquals("First Value Not 3", 3, value);
+
 	}
 
+	@Test
+	public void test_Predictable_Roll_2()
+	{
+		Die die = new Die(new int[]
+		{ 3, 2, 1 });
+		die.roll();
+		assertEquals("First value Not 3", 3, die.getLastRoll());
+
+		die.roll();
+		assertEquals("First value Not 3", 2, die.getLastRoll());
+
+	}
 	
+	@Test
+	public void test_Predictable_Roll_3()
+	{
+		Die die = new Die(new int[]
+		{ 3, 2, 1 });
+		die.roll();
+		die.roll();
+		die.roll();
+		assertEquals("First value Not 1", 1, die.getLastRoll());
+
+	}
+	
+	@Test
+	public void test_Predictable_Roll_4_with_3_values()
+	{
+		Die die = new Die(new int[]
+		{ 3, 2, 1 });
+		die.roll();
+		die.roll();
+		die.roll();
+		die.roll();
+		assertEquals("First value Not going back to first value 3", 3, die.getLastRoll());
+
+	}
+
 }
