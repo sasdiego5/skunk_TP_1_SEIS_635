5e6fe5a8628b8a4f53765a389550118bd608d6dd
cs563 <allnightlong92@yahoo.com>
2018-11-01 21:44:45 -0500

    testing

---
+++ SkunkProject/test/TestGame.java
@@ -0,0 +1,148 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGame {
+
+	
+	@Test
+	public void testGetRound() {
+		this.round = new Round();
+		String TestRound = this.round.getRound();
+		assertEquals("This round is the correct rounf",TestRound);
+
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartNewTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayersNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetPlayersNum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPlayerName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCurrentPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNextPlayerTurn() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextTurnIndex() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTurnNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWinner() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEnds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDisplayRules() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
