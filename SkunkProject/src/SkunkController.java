1cc4ee79433ba10d08ea1a9e344d5cea0f8207c2
Diego F Orejuela <diegoforejuelao@gmail.com>
2018-11-30 13:49:17 -0600

    separated UI and domain layer, by adding skunkController app

---
+++ SkunkProject/src/SkunkController.java
@@ -0,0 +1,160 @@
+import java.io.IOException;
+import java.util.ArrayList;
+
+import edu.princeton.cs.introcs.StdIn;
+import edu.princeton.cs.introcs.StdOut;
+
+public class SkunkController implements Reporter
+{
+	public Game game;
+
+	public SkunkController()
+	{
+		this.game = new Game();
+	}
+
+	public boolean run() throws IOException
+	{
+		StdOut.println("**************************************************************************");
+		StdOut.println("Welcome to 635 Skunk project!");
+		StdOut.println("**************************************************************************");
+
+		int numPlayers = 0;
+		while (numPlayers < 2)
+		{
+			StdOut.println("Enter number of players: ");
+			numPlayers = StdIn.readInt();
+			StdIn.readLine();
+			game.setPlayersNum(numPlayers);
+		}
+
+		game.addPlayer();
+
+		StdOut.println("**************************************************************************");
+
+		StdOut.println("Do you want to view the rules of the game? Enter: yes or no");
+		String viewRules = StdIn.readLine();
+		if (viewRules.equalsIgnoreCase("y"))
+		{
+			game.displayRules();
+		}
+
+		StdOut.println("**************************************************************************");
+		StdOut.println("Let The Game Begin!");
+		StdOut.println("**************************************************************************");
+
+		while (!game.win()) // play as many turns until a player scores 100 or
+							// more
+		{
+			String player = game.getPlayerName();
+
+			StdOut.println("Turn " + game.getTurnNumber());
+
+			StdOut.println("Player in Turn to Roll the dice: " + player);
+
+			while (true)
+			{
+				StdOut.println(player + ", Do you want to roll the dice? yes or no?");
+
+				String response = StdIn.readLine();
+
+				if (!response.equalsIgnoreCase("no"))
+				{
+					game.getRound().rollAgain();
+					game.getRound().scoreTurn();
+					StdOut.println(game.getRound().getDiceVals());
+					StdOut.println(player + " rolled a " + game.getRound().getLastRoll().getDice().getLastRoll());
+
+					if (game.ends())
+						break;
+				}
+				else
+				{
+					StdOut.println(player + " declined to roll.");
+					break;
+				}
+			}
+
+			int turnScore = game.getRound().getTurnScore();
+			game.getCurrentPlayer().setScore(turnScore);
+			game.penalties();
+			StdOut.println(player + " scored " + turnScore + " points in this turn.");
+			StdOut.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+			StdOut.println(game.getCurrentPlayer());
+
+			game.nextPlayerTurn();
+			game.getRound().startNewTurn();
+
+		}
+
+		StdOut.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		StdOut.println(game.getWinner().getName() + " has reached a winning score");
+		StdOut.println("Each of the other players get one more turn!");
+
+		// extra turn for players to increase the score, exclude the player who
+		// scored 100 or more
+		int tempTurns = 1;
+		while (tempTurns <= game.getPlayersNum() - 1)
+		{
+			while (true)
+			{
+				StdOut.println(game.getCurrentPlayer().getName() + ", Do you want to roll the dice? yes or no?");
+
+				String response = StdIn.readLine();
+
+				if (!response.equalsIgnoreCase("no"))
+				{
+					game.getRound().rollAgain();
+					game.getRound().scoreTurn();
+					StdOut.println(game.getRound().getDiceVals());
+					StdOut.println(game.getCurrentPlayer().getName() + " rolled a "
+							+ game.getRound().getLastRoll().getDice().getLastRoll());
+
+					if (game.ends())
+						break;
+				}
+				else
+				{
+					StdOut.println(game.getCurrentPlayer().getName() + " declined to roll.");
+					break;
+				}
+
+			}
+			
+			int turnScore = game.getRound().getTurnScore();
+			game.getCurrentPlayer().setScore(turnScore);
+			game.penalties();
+			StdOut.println(game.getCurrentPlayer().getName() + " scored " + turnScore + " points in this turn.");
+			StdOut.println("++++++++++++++++++++++++++++++++++++++++++++++++++++");
+			StdOut.println(game.getCurrentPlayer());
+
+			game.nextPlayerTurn();
+			game.getRound().startNewTurn();
+			
+			if (game.getCurrentPlayer().getScore() >= 100)
+			{
+				game.nextPlayerTurn();
+				game.getRound().startNewTurn();
+			}
+
+			tempTurns++;
+
+		}
+
+		game.win();
+		StdOut.println();
+		StdOut.println("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		StdOut.println(game.getWinner().getName() + " is the Winner of this round!");
+		game.winKitty();
+		game.gameStats();
+		
+		return true;
+	}
+
+	@Override
+	public void showMessage(String msg)
+	{
+		StdOut.println(msg);
+	}
+
+}
--- SkunkProject/test/TestGame.java
+++ SkunkProject/test/TestGame.java
@@ -5,117 +5,107 @@ import org.junit.Before;
 
 import org.junit.Test;
 
-public class TestGame {
+public class TestGame
+{
 
 	private Game game;
-	
+
 	@Before
 	public void setUp() throws Exception
 	{
-	this.game = new Game();
+		this.game = new Game();
 	}
 
 	@After
 	public void tearDown() throws Exception
 	{
 	}
-	
+
 	@Test
-	public void test_getRound() {
-		
-		//this.round = new Round();
-		//String TestRound = this.round.getRound();
-		//assertEquals("This round is the correct round ",TestRound);
+	public void test_getRound()
+	{
+
+		// this.round = new Round();
+		// String TestRound = this.round.getRound();
+		// assertEquals("This round is the correct round ",TestRound);
 	}
-	
+
 	@Test
-	public void test_3_players() {
+	public void test_3_players()
+	{
 		game.setPlayersNum(3);
 		int value = game.getPlayersNum();
 		assertEquals("There are ", 3, value);
 	}
 
-
-//	@Test
-//	public void testStartNewTurn() {
-//		fail("Not yet implemented");
-//	}
+	// @Test
+	// public void testStartNewTurn() {
+	// fail("Not yet implemented");
+	// }
 
 	@Test
-	public void test_getPlayersNum() {
+	public void test_getPlayersNum()
+	{
 		game.setPlayersNum(4);
 		int value = game.getPlayersNum();
 		assertEquals("There are ", 4, value);
 	}
 
 	@Test
-	public void testSetPlayersNum() {
+	public void testSetPlayersNum()
+	{
 		game.setPlayersNum(1);
 		int value = game.getPlayersNum();
 		assertEquals("There are ", 1, value);
 	}
 
-
-
 	@Test
-	public void test_getPlayerName() {
-		game.setPlayersNum(1);
-		game.addPlayer();
-		
-		String name = game.getPlayerName();
-		assertEquals("The player's name is ", "Tom", name);
-	}
+	public void testGetCurrentPlayer()
+	{
 
-	@Test
-	public void testGetCurrentPlayer() {
-	
 		Player turnPlayer = new Player("");
 		Player currentPlayer = game.getCurrentPlayer();
 		assertEquals("The current player is ", "Tom", currentPlayer);
 	}
-//
-//	@Test
-//	public void testNextPlayerTurn() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetNextTurnIndex() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetTurnNumber() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testWin() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testWinner() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testEnds() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testDisplayRules() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetClass() {
-//		fail("Not yet implemented");
-//	}
-
-	
-
-	
+	//
+	// @Test
+	// public void testNextPlayerTurn() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetNextTurnIndex() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetTurnNumber() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testWin() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testWinner() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testEnds() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testDisplayRules() {
+	// fail("Not yet implemented");
+	// }
+	//
+	// @Test
+	// public void testGetClass() {
+	// fail("Not yet implemented");
+	// }
 
 }
